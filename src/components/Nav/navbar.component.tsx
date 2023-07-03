e6d453b08b83c76147671fbd837d141d199462d9
Haruka <hfujii1013@gmail.com>
2023-07-03 13:07:39 -0700

    edited logo

--- src/components/Nav/navbar.component.tsx
+++ src/components/Nav/navbar.component.tsx
@@ -4,7 +4,13 @@ export default function Navbar() {
   return (
     <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bg-light px-4">
       <div className="max-w-container h-full mx-auto py-1 flex items-center justify-between">
-        <div></div>
+        <div>
+          <img
+            className="w-14"
+            src={'/img/logo.svg'}
+            alt="login page picture"
+          />
+        </div>
         <div></div>
       </div>
     </div>
