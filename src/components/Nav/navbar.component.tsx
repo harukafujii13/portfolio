166f0294dea768938b21e4cbefd5ee69b6351dcb
Haruka <hfujii1013@gmail.com>
2023-07-03 00:54:50 -0700

    added css

--- src/components/Nav/navbar.component.tsx
+++ src/components/Nav/navbar.component.tsx
@@ -1,5 +1,12 @@
 'use client';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bg-light px-4">
+      <div className="max-w-container h-full mx-auto py-1 flex items-center justify-between">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
