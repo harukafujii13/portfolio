835145eb7eb156b6832c4bfa710e57877e5aeb8f
Haruka <hfujii1013@gmail.com>
2023-07-05 13:53:08 -0700

    added css

--- src/components/Nav/navbar.component.tsx
+++ src/components/Nav/navbar.component.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 
 export default function Navbar() {
   return (
-    <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bg-light px-4 font-init">
+    <div className="w-full shadow-navbarShadow h-20 lg:h-[12vh] sticky top-0 z-50 bg-bg-light px-4 font-inter">
       <div className="max-w-container h-full mx-auto py-1 flex items-center justify-between">
         <motion.div
           initial={{ opacity: 0 }}
@@ -19,10 +19,10 @@ export default function Navbar() {
         </motion.div>
 
         <div className="hidden mdl:inline-flex items-center gap-7">
-          <ul className="flex text-[1rem] gap-7">
+          <ul className="flex text-[1.1rem] gap-7">
             <Link
               href="#home"
-              className="flex items-center gap-1 font-medium text-primary-gray hover:text-primary-purple cursor-pointer duration-300 nav-link font-medium">
+              className="flex items-center gap-1 font-medium text-primary-gray hover:text-primary-purple cursor-pointer duration-300 nav-link font-bold">
               <motion.li
                 initial={{ y: -10, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
@@ -33,7 +33,7 @@ export default function Navbar() {
 
             <Link
               href="#about"
-              className="flex items-center gap-1 font-medium text-primary-gray hover:text-primary-purple cursor-pointer duration-300 nav-link font-medium">
+              className="flex items-center gap-1 font-medium text-primary-gray hover:text-primary-purple cursor-pointer duration-300 nav-link font-bold">
               <motion.li
                 initial={{ y: -10, opacity: 0 }}
                 animate={{ y: 0.1, opacity: 1 }}
@@ -44,7 +44,7 @@ export default function Navbar() {
 
             <Link
               href="#skills"
-              className="flex items-center gap-1 font-medium text-primary-gray hover:text-primary-purple cursor-pointer duration-300 nav-link font-medium">
+              className="flex items-center gap-1 font-medium text-primary-gray hover:text-primary-purple cursor-pointer duration-300 nav-link font-bold">
               <motion.li
                 initial={{ y: -10, opacity: 0 }}
                 animate={{ y: 0.1, opacity: 1 }}
@@ -55,7 +55,7 @@ export default function Navbar() {
 
             <Link
               href="#Works"
-              className="flex items-center gap-1 font-medium text-primary-gray hover:text-primary-purple cursor-pointer duration-300 nav-link font-medium">
+              className="flex items-center gap-1 font-medium text-primary-gray hover:text-primary-purple cursor-pointer duration-300 nav-link font-bold">
               <motion.li
                 initial={{ y: -10, opacity: 0 }}
                 animate={{ y: 0.1, opacity: 1 }}
@@ -66,7 +66,7 @@ export default function Navbar() {
 
             <Link
               href="#Contact"
-              className="flex items-center gap-1 font-medium text-primary-gray hover:text-primary-purple cursor-pointer duration-300 nav-link font-medium">
+              className="flex items-center gap-1 font-medium text-primary-gray hover:text-primary-purple cursor-pointer duration-300 nav-link font-bold">
               <motion.li
                 initial={{ y: -10, opacity: 0 }}
                 animate={{ y: 0.1, opacity: 1 }}
@@ -83,7 +83,7 @@ export default function Navbar() {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ delay: 0.5 }}
-              className="px-4 py-2 rounded-md text-primary-purple text-[1rem] border border-primary-purple hover:bg-hoverColor duration-300 font-medium">
+              className="px-4 py-2 rounded-md text-primary-purple text-[1.1rem] border border-primary-purple hover:bg-hoverColor duration-300 font-bold">
               Resume
             </motion.button>
           </a>
