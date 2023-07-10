8eb437e22b48312661af7fc9f06d859ba07d34a5
Haruka <hfujii1013@gmail.com>
2023-07-09 19:02:09 -0700

    feat: left side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import LeftSide from '@/components/MainLeftside/leftside.component';
 import Navbar from '@/components/Nav/navbar.component';
 
 export default function Home() {
   return (
     <main className="w-full h-screen font-inter bg-bg-light text-primary-gray overflow-x-hidden overflow-y-scroll">
       <Navbar />
+      <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
+        <LeftSide />
+        <div></div>
+        <div></div>
+      </div>
     </main>
   );
 }
