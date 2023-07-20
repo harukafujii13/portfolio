bb1bf545b2c8913aebdaf8a123b45b0d4080db28
Haruka <hfujii1013@gmail.com>
2023-07-20 00:26:44 -0700

    feat: create about work

---
+++ src/components/Works/works.component.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SectionTitle from '../SectionTitle/sectionTitle.component';
+
+const Works = () => {
+  return (
+    <section
+      id="works"
+      className="max-w-containerSmall mx-auto py-10 lgl:py-32">
+      <SectionTitle
+        title="Works"
+        titleNo="03"
+      />
+    </section>
+  );
+};
+
+export default Works;
