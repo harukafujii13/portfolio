d9ae4fe39bacceac453a6f11c93a40437c3c300f
Haruka <hfujii1013@gmail.com>
2023-07-19 17:58:56 -0700

    feat: change css

--- src/components/About/about.component.tsx
+++ src/components/About/about.component.tsx
@@ -63,10 +63,10 @@ function About() {
               />
               <div className="hidden lgl:inline-block absolute w-full h-80 rounded-md top-0 left-0 group-hover:bg-transparent duration-300">
                 {' '}
-                <div className="hidden lgl:inline-flex w-full h-80 border-2 border-primary-green rounded-md group-hover:translate-x-2 group-hover:translate-y-2 transition-transform duration-300"></div>
               </div>
             </div>
           </div>
+          <div className="hidden lgl:inline-flex w-full h-80 border-2 bg-primary-green border-primary-purple rounded-md group-hover:translate-x-2 group-hover:translate-y-2 transition-transform duration-300"></div>
         </div>
       </div>
     </section>
