7b6d0f95cc50b09e63f378a5cc2d81b3566db29b
Haruka <hfujii1013@gmail.com>
2023-07-23 11:37:57 -0700

    feat: add event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ import Footer from '@/components/Footer/footer.component';
 export default function Home() {
   return (
     <main
-      className="w-full h-screen font-inter bg-bg-light text-primary-text overflow-x-hidden overflow-y-scroll scrollbar scrollbar-track-light
-    scrollbar-thumb-primary-text/60">
+      className="w-full h-screen font-inter bg-bg-light text-primary-text overflow-x-hidden overflow-y-scroll 
+    ">
       <Navbar />
       <div className="w-full h-[88vh] xl:flex items-center gap-20 justify-between">
         <motion.div
