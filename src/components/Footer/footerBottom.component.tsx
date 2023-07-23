69f50cd516fc6ece0fa0ca00cde67d95dbb37a9c
Haruka <hfujii1013@gmail.com>
2023-07-23 16:06:05 -0700

    feat: create footer bottom

---
+++ src/components/Footer/footerBottom.component.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FooterBottom = () => {
+  return (
+    <div className="font-inter flex justify-center items-center h-12">
+      <p className="text-sm text-primary-text">
+        &copy; 2023 Haruka Fujii. All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default FooterBottom;
