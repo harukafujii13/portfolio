61afc1abc2846c3973f207bc686a2fd3e712300e
Haruka <hfujii1013@gmail.com>
2023-09-09 14:31:47 -0700

    update: change style

--- src/components/Works/works.component.tsx
+++ src/components/Works/works.component.tsx
@@ -15,190 +15,198 @@ const Works = () => {
       {/* Projects container */}
       <div className="w-full flex flex-col items-center gap-28 mt-10 px-5 md:px-0">
         {/* Project one*/}
-        <div className="w-full flex flex-col lg:flex-row items-center gap-6">
-          <a
-            className="w-full lg:w-1/2 h-auto relative group mb-6 lg:mb-0"
-            href="https://cafe-hopping.vercel.app/"
-            target="_blank"
-            rel="noopener noreferrer">
-            <img
-              className="w-full h-full object-cover rounded-md shadow-lg"
-              src={'/img/cafe_hopping.png'}
-              alt="work"
-            />
-          </a>
-          <div className="w-full lg:w-1/2 flex flex-col gap-6">
-            <h3 className="text-2xl font-extrabold text-primary-purple font-montserrat">
-              Cafe Hopping
-            </h3>
-            <p className="bg-primary-pink text-sm md:text-base p-2 md:p-6 rounded-md font-inter">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              rem quia obcaecati et provident cum, fugiat numquam soluta,
-              perspiciatis magnam non nulla eos tempore expedita at, asperiores
-              ea veritatis eum.
-            </p>
-            <ul className="text-xs md:text-sm font-inter tracking-wide flex flex-wrap gap-2 md:gap-5 justify-start text-primary-text">
-              <li>Nextjs</li>
-              <li>Typescript</li>
-              <li>Next-auth</li>
-              <li>Tailwind</li>
-            </ul>
-            <div className="text-2xl flex gap-4">
-              <a
-                href="https://github.com/harukafujii13/CafeHopping_App.git"
-                className="hover:text-primary-purple duration-300"
-                target="_blank"
-                rel="noopener noreferrer">
-                <TbBrandGithubFilled />
-              </a>
-              <a
-                href="https://cafe-hopping.vercel.app/"
-                className="hover:text-primary-purple duration-300"
-                target="_blank"
-                rel="noopener noreferrer">
-                <RxOpenInNewWindow />
-              </a>
+        <div className="flex flex-col gap-6">
+          <div className="text-2xl font-extrabold text-primary-purple font-montserrat flex justify-center">
+            Cafe Hopping
+          </div>
+          <div className="w-full flex flex-col lg:flex-row items-center gap-6">
+            <a
+              className="w-full lg:w-1/2 h-auto relative group mb-6 lg:mb-0"
+              href="https://cafe-hopping.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer">
+              <img
+                className="w-full h-full object-cover rounded-md shadow-lg"
+                src={'/img/cafe_hopping.png'}
+                alt="work"
+              />
+            </a>
+            <div className="w-full lg:w-1/2 flex flex-col gap-6">
+              <p className="bg-primary-pink text-sm md:text-base p-2 md:p-6 rounded-md font-inter">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias rem quia obcaecati et provident cum, fugiat numquam
+                soluta, perspiciatis magnam non nulla eos tempore expedita at,
+                asperiores ea veritatis eum.
+              </p>
+              <ul className="text-xs md:text-sm font-inter tracking-wide flex flex-wrap gap-2 md:gap-5 justify-start text-primary-text">
+                <li>Nextjs</li>
+                <li>Typescript</li>
+                <li>Next-auth</li>
+                <li>Tailwind</li>
+              </ul>
+              <div className="text-2xl flex gap-4">
+                <a
+                  href="https://github.com/harukafujii13/CafeHopping_App.git"
+                  className="hover:text-primary-purple duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <TbBrandGithubFilled />
+                </a>
+                <a
+                  href="https://cafe-hopping.vercel.app/"
+                  className="hover:text-primary-purple duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <RxOpenInNewWindow />
+                </a>
+              </div>
             </div>
           </div>
         </div>
         {/* Project two*/}
-        <div className="w-full flex flex-col lg:flex-row-reverse items-center gap-6 mt-10">
-          <a
-            className="w-full lg:w-1/2 h-auto relative group mb-6 lg:mb-0"
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer">
-            <img
-              className="w-full h-full object-cover rounded-md shadow-lg"
-              src={'/img/money_note.png'}
-              alt="work"
-            />
-          </a>
-          <div className="w-full lg:w-1/2 flex flex-col gap-6">
-            <h3 className="text-2xl font-extrabold text-primary-purple font-montserrat">
-              Money Note
-            </h3>
-            <p className="bg-primary-green text-sm md:text-base p-2 md:p-6 rounded-md font-inter">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              rem quia obcaecati et provident cum, fugiat numquam soluta,
-              perspiciatis magnam non nulla eos tempore expedita at, asperiores
-              ea veritatis eum.
-            </p>
-            <ul className="text-xs md:text-sm font-inter tracking-wide flex flex-wrap gap-2 md:gap-5 justify-start text-primary-text">
-              <li>Nextjs</li>
-              <li>Typescript</li>
-              <li>Next-auth</li>
-              <li>Tailwind</li>
-            </ul>
-            <div className="text-2xl flex gap-4">
-              <a
-                href="https://github.com/harukafujii13/MoneyNote_App.git"
-                className="hover:text-primary-purple duration-300"
-                target="_blank"
-                rel="noopener noreferrer">
-                <TbBrandGithubFilled />
-              </a>
-              <a
-                href="#"
-                className="hover:text-primary-purple duration-300"
-                target="_blank"
-                rel="noopener noreferrer">
-                <RxOpenInNewWindow />
-              </a>
+        <div className="flex flex-col gap-6">
+          <div className="text-2xl font-extrabold text-primary-purple font-montserrat flex justify-center">
+            Money Note
+          </div>
+          <div className="w-full flex flex-col lg:flex-row-reverse items-center gap-6">
+            <a
+              className="w-full lg:w-1/2 h-auto relative group mb-6 lg:mb-0"
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer">
+              <img
+                className="w-full h-full object-cover rounded-md shadow-lg"
+                src={'/img/money_note.png'}
+                alt="work"
+              />
+            </a>
+            <div className="w-full lg:w-1/2 flex flex-col gap-6">
+              <p className="bg-primary-green text-sm md:text-base p-2 md:p-6 rounded-md font-inter">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias rem quia obcaecati et provident cum, fugiat numquam
+                soluta, perspiciatis magnam non nulla eos tempore expedita at,
+                asperiores ea veritatis eum.
+              </p>
+              <ul className="text-xs md:text-sm font-inter tracking-wide flex flex-wrap gap-2 md:gap-5 justify-start text-primary-text">
+                <li>Nextjs</li>
+                <li>Typescript</li>
+                <li>Next-auth</li>
+                <li>Tailwind</li>
+              </ul>
+              <div className="text-2xl flex gap-4">
+                <a
+                  href="https://github.com/harukafujii13/MoneyNote_App.git"
+                  className="hover:text-primary-purple duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <TbBrandGithubFilled />
+                </a>
+                <a
+                  href="#"
+                  className="hover:text-primary-purple duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <RxOpenInNewWindow />
+                </a>
+              </div>
             </div>
           </div>
         </div>
         {/* Project3 */}
-        <div className="w-full flex flex-col lg:flex-row items-center gap-6">
-          <a
-            className="w-full lg:w-1/2 h-auto relative group mb-6 lg:mb-0"
-            href="https://teddies-frontend.vercel.app/"
-            target="_blank"
-            rel="noopener noreferrer">
-            <img
-              className="w-full h-full object-cover rounded-md shadow-lg"
-              src={'/img/teddy.png'}
-              alt="work"
-            />
-          </a>
-          <div className="w-full lg:w-1/2 flex flex-col gap-6">
-            <h3 className="text-2xl font-extrabold text-primary-purple font-montserrat">
-              Teddies vs Monsters
-            </h3>
-            <p className="bg-primary-pink text-sm md:text-base p-2 md:p-6 rounded-md font-inter">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              rem quia obcaecati et provident cum, fugiat numquam soluta,
-              perspiciatis magnam non nulla eos tempore expedita at, asperiores
-              ea veritatis eum.
-            </p>
-            <ul className="text-xs md:text-sm font-inter tracking-wide flex flex-wrap gap-2 md:gap-5 justify-start text-primary-text">
-              <li>Nextjs</li>
-              <li>Typescript</li>
-              <li>Next-auth</li>
-              <li>Tailwind</li>
-            </ul>
-            <div className="text-2xl flex gap-4">
-              <a
-                href="https://github.com/jun-tsuno/teddies_frontend.git"
-                className="hover:text-primary-purple duration-300"
-                target="_blank"
-                rel="noopener noreferrer">
-                <TbBrandGithubFilled />
-              </a>
-              <a
-                href="https://teddies-frontend.vercel.app/"
-                className="hover:text-primary-purple duration-300"
-                target="_blank"
-                rel="noopener noreferrer">
-                <RxOpenInNewWindow />
-              </a>
+        <div className="flex flex-col gap-6">
+          <div className="text-2xl font-extrabold text-primary-purple font-montserrat flex justify-center">
+            Teddies vs Monsters
+          </div>
+          <div className="w-full flex flex-col lg:flex-row items-center gap-6">
+            <a
+              className="w-full lg:w-1/2 h-auto relative group mb-6 lg:mb-0"
+              href="https://teddies-frontend.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer">
+              <img
+                className="w-full h-full object-cover rounded-md shadow-lg"
+                src={'/img/teddy.png'}
+                alt="work"
+              />
+            </a>
+            <div className="w-full lg:w-1/2 flex flex-col gap-6">
+              <p className="bg-primary-pink text-sm md:text-base p-2 md:p-6 rounded-md font-inter">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias rem quia obcaecati et provident cum, fugiat numquam
+                soluta, perspiciatis magnam non nulla eos tempore expedita at,
+                asperiores ea veritatis eum.
+              </p>
+              <ul className="text-xs md:text-sm font-inter tracking-wide flex flex-wrap gap-2 md:gap-5 justify-start text-primary-text">
+                <li>Nextjs</li>
+                <li>Typescript</li>
+                <li>Next-auth</li>
+                <li>Tailwind</li>
+              </ul>
+              <div className="text-2xl flex gap-4">
+                <a
+                  href="https://github.com/jun-tsuno/teddies_frontend.git"
+                  className="hover:text-primary-purple duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <TbBrandGithubFilled />
+                </a>
+                <a
+                  href="https://teddies-frontend.vercel.app/"
+                  className="hover:text-primary-purple duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <RxOpenInNewWindow />
+                </a>
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="w-full flex flex-col lg:flex-row-reverse items-center gap-6 mt-10">
-          <a
-            className="w-full lg:w-1/2 h-auto relative group mb-6 lg:mb-0"
-            href="https://aurora-forecast.vercel.app/"
-            target="_blank"
-            rel="noopener noreferrer">
-            <img
-              className="w-full h-full object-cover rounded-md shadow-lg"
-              src={'/img/aurora.png'}
-              alt="work"
-            />
-          </a>
-          <div className="w-full lg:w-1/2 flex flex-col gap-6">
-            <h3 className="text-2xl font-extrabold text-primary-purple font-montserrat">
-              Aurora Forecast
-            </h3>
-            <p className="bg-primary-green text-sm md:text-base p-2 md:p-6 rounded-md font-inter">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
-              rem quia obcaecati et provident cum, fugiat numquam soluta,
-              perspiciatis magnam non nulla eos tempore expedita at, asperiores
-              ea veritatis eum.
-            </p>
-            <ul className="text-xs md:text-sm font-inter tracking-wide flex flex-wrap gap-2 md:gap-5 justify-start text-primary-text">
-              <li>Nextjs</li>
-              <li>Typescript</li>
-              <li>Next-auth</li>
-              <li>Tailwind</li>
-            </ul>
-            <div className="text-2xl flex gap-4">
-              <a
-                href="https://github.com/harukafujii13/Aurora_Forecast.git"
-                className="hover:text-primary-purple duration-300"
-                target="_blank"
-                rel="noopener noreferrer">
-                <TbBrandGithubFilled />
-              </a>
-              <a
-                href="https://aurora-forecast.vercel.app/"
-                className="hover:text-primary-purple duration-300"
-                target="_blank"
-                rel="noopener noreferrer">
-                <RxOpenInNewWindow />
-              </a>
+        <div className="flex flex-col gap-6">
+          <h3 className="text-2xl font-extrabold text-primary-purple font-montserrat flex justify-center">
+            Aurora Forecast
+          </h3>
+          <div className="w-full flex flex-col lg:flex-row-reverse items-center gap-6">
+            <a
+              className="w-full lg:w-1/2 h-auto relative group mb-6 lg:mb-0"
+              href="https://aurora-forecast.vercel.app/"
+              target="_blank"
+              rel="noopener noreferrer">
+              <img
+                className="w-full h-full object-cover rounded-md shadow-lg"
+                src={'/img/aurora.png'}
+                alt="work"
+              />
+            </a>
+            <div className="w-full lg:w-1/2 flex flex-col gap-6">
+              <p className="bg-primary-green text-sm md:text-base p-2 md:p-6 rounded-md font-inter">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias rem quia obcaecati et provident cum, fugiat numquam
+                soluta, perspiciatis magnam non nulla eos tempore expedita at,
+                asperiores ea veritatis eum.
+              </p>
+              <ul className="text-xs md:text-sm font-inter tracking-wide flex flex-wrap gap-2 md:gap-5 justify-start text-primary-text">
+                <li>Nextjs</li>
+                <li>Typescript</li>
+                <li>Next-auth</li>
+                <li>Tailwind</li>
+              </ul>
+              <div className="text-2xl flex gap-4">
+                <a
+                  href="https://github.com/harukafujii13/Aurora_Forecast.git"
+                  className="hover:text-primary-purple duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <TbBrandGithubFilled />
+                </a>
+                <a
+                  href="https://aurora-forecast.vercel.app/"
+                  className="hover:text-primary-purple duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  <RxOpenInNewWindow />
+                </a>
+              </div>
             </div>
           </div>
         </div>
